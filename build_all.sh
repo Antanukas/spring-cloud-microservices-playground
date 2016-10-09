436f4926b000556841f108681d73fd8249ad61ee
Antanas Bastys <antanas.bastys@tieto.com>
2016-10-09 18:37:26 +0300

    First commit

---
+++ build_all.sh
@@ -0,0 +1,3 @@
+cd tpay-accounts-service && ./gradlew clean build && cd ..
+cd tpay-config-server && ./gradlew clean build && cd ..
+cd tpay-service-discovery && ./gradlew clean build && cd ..
---
+++ tpay-service-discovery/src/main/java/com/tpay/TpayServiceDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.tpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TpayServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpayServiceDiscoveryApplication.class, args);
+	}
+}
