6750ce1d4a949becd3490b8ac64a2982955a219a
Antanas Bastys <antanas.bastys@tieto.com>
2016-10-11 11:59:10 +0300

    Added Zuul API Gateway

--- tpay-accounts-service/src/main/java/com/tpay/AccountsServiceApplication.java
+++ tpay-accounts-service/src/main/java/com/tpay/AccountsServiceApplication.java
@@ -3,10 +3,18 @@ package com.tpay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class AccountsServiceApplication {
+
+	@GetMapping("/")
+	public String hi() {
+		return "Hi";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsServiceApplication.class, args);
---
+++ tpay-api-gateway/src/main/java/com/tpay/ApiGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.tpay;
+
+import com.netflix.zuul.ZuulFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+    @Bean
+    public ZuulFilter loggingFilter() {
+        return new RequestLoggingFilter();
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
